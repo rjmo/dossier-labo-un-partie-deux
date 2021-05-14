6ca65a7ad1233c636f8bd177febfc62a5c2fe1fa
rjmo <radiorafael@hotmail.com>
2021-05-14 17:46:09 -0400

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function montrer(idElem) {
+  document.getElementById(idElem).style.display = "block";
+}
+function cacher(idElem) {
+  document.getElementById(idElem).style.display = "none";
+}
+function pause(idvideo) {
+  document.getElementById(idvideo).pause();
+}
+function play(idvideo) {
+  document.getElementById(idvideo).play();
+}
